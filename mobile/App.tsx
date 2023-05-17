bccb39280bda424bb02f488376b1dcd89a611880
Lucas Leonardo <barss.lucas@gmail.com>
2023-05-17 13:35:19 -0300

    Setup tailwind to react-name (nativewind)

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -3,8 +3,8 @@ import { Text, View } from 'react-native'
 
 export default function App() {
   return (
-    <View>
-      <Text>Hello, World!</Text>
+    <View className="flex-1 items-center justify-center bg-zinc-950">
+      <Text className="text-zinc-50 font-bold text-6xl">Hello, World!</Text>
       <StatusBar style="light" translucent />
     </View>
   )
